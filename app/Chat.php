0881bfde96e67974417ca684e5459f6e50674bbe
kocsburak <burak_s96@hotmail.com>
2019-10-26 02:14:59 +0300

    update

--- app/Chat.php
+++ app/Chat.php
@@ -7,4 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Chat extends Model
 {
     //
+    protected $fillable = [
+      'user_id'
+    ];
 }
--- app/Match.php
+++ app/Match.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Match extends Model
 {
     protected $table = "events";
+
+    protected $fillable = [
+      'user_id','group','shuffle_count','is_online'
+    ];
 }
