93be134c5cbe67a6ceef7c338db8bd33be0b148d
kocsburak <burak_s96@hotmail.com>
2019-11-15 03:39:03 +0300

    update

--- app/University.php
+++ app/University.php
@@ -1,33 +1,22 @@
 <?php
 
 namespace App;
-use Illuminate\Database\Eloquent\Model;
 
-/**
-* Created By Burak on 09/10/2019
-**/
+use Illuminate\Database\Eloquent\Model;
 
 class University extends Model
 {
     //
-
-   protected $table = "universities";
-
-   protected $fillable = [
-       'id','name'
-   ];
+protected $table = "universities";
+protected $fillable = ['id','name'];
 
 
-   /**
-   * departments() -> Üniversiteye Ait Olan Tüm Bölümleri Getiren Fonksiyon
-   **/
-
-/*
     public function departments(){
 
       return $this->belongsToMany('App\Department','university_departments','university_id','department_id');
 
     }
-*/
+
+
 
 }
