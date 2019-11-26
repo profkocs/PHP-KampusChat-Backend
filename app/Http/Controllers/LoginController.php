6d1e8393a9246e8843475f9a45e1696ad30311bc
kocsburak <burak_s96@hotmail.com>
2019-11-26 15:31:53 +0300

    udapte

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -1,4 +1,8 @@
 <?php
+
+use App\Http\Controllers\Controller;
+
+
 use Illuminate\Http\Request;
 use Illuminate\Routing\Route;
 use Illuminate\Support\Facades\Validator;
@@ -8,37 +12,54 @@ use Laravel\Passport\Client;
 class LoginController extends Controller {
 
 
-private $client;
-public function __construct(){
-  $this->client = Client::find(3);
-}
 
-public function login(){
+    private $client;
+    public function __construct(){
+        $this->client = Client::find(3);
+    }
 
-  $validator = Validator::make($request->all(), [
-      'password' => 'required',
-      'username' => 'required',
-  ]);
+    public function login(Request $request){
 
-  $input = $request->all();
+        $validator = Validator::make($request->all(), [
+            'password' => 'required',
+            'username' => 'required',
+        ]);
 
-  $params = [
-     'grant_type' => 'password',
-     'client_id' => $this->client->id,
-     'client_secret' => $this->client->secret,
-     'username' => $input['username'],
-     'password' => $input['password'],
-     'scope' => '*'
-  ];
+        $input = $request->all();
 
-
-  $request->request->add($params);
-  $proxy = Request::create('oauth/token','POST');
-  return Route::dispatch($proxy);
+        $params = [
+            'grant_type' => 'password',
+            'client_id' => $this->client->id,
+            'client_secret' => $this->client->secret,
+            'username' => $input['username'],
+            'password' => $input['password'],
+            'scope' => '*'
+        ];
 
 
+        $request->request->add($params);
+        $proxy = Request::create('oauth/token','POST');
+        return Route::dispatch($proxy);
 
-}
+
+
+    }
+
+    public function logout(Request $request){
+        $accessToken = Auth::user()->token();
+        DB::table('oaunt_refresh_tokens')->where('access_token_id',$accessToken->id)->update(['revoked'=>true]);
+        $accessToken->revoke();
+        return response()->json("OK",204);
+    }
+
+    public function forgotPassword(Request $request){
+// email kodu gönder
+    }
+
+
+    public function verifyCode(Request $request){
+        // kodu doğrula
+    }
 
 
 
