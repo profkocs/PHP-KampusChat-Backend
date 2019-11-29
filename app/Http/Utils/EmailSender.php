bc0335ba293abb4da324593a5afa2469153bc5e0
kocsburak <burak_s96@hotmail.com>
2019-11-29 19:05:58 +0300

    Update

--- app/Http/Utils/EmailSender.php
+++ app/Http/Utils/EmailSender.php
@@ -23,7 +23,7 @@ class EmailSender
             $message->from('simpleappvision@gmail.com', 'KampusChat Your Process Code');
         });
 
-        Code::create(['email' => $email], ['code' => $code], ['type' => $type], ['revoked' => false]);
+        Code::updateOCreate(['email' => $email , 'code' => $code , 'type' => $type , 'revoked' => false]);
 
 
     }
