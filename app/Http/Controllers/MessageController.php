a6e3944f53b620d3a1f77cf21cf996fc9102f0c8
kocsburak <burak_s96@hotmail.com>
2020-03-24 18:02:36 +0300

    Hatalar Düzeltildi.

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -14,18 +14,17 @@ class MessageController extends Controller
     //
 
 
-    public function checkNewMessages($user_id,$last_date)
+    public function checkNewMessages($user_id, $last_date)
     {
-
 
 
         $messages = new Collection();
         $chats = Chat::where('owner_user_id', $user_id)->orWhere('guest_user_id', $user_id)->get();
         foreach ($chats as $chat) {
 
-            $tmessages = \App\Message::where('chat_id', $chat->id)->where('sender_user_id','!=',$user_id)->where('is_seen', false)->where('created_at','>',$last_date)->get();
+            $tmessages = \App\Message::where('chat_id', $chat->id)->where('sender_user_id', '!=', $user_id)->where('is_seen', false)->where('created_at', '>', $last_date)->get();
 
-            foreach($tmessages as $message){
+            foreach ($tmessages as $message) {
                 $messages->push($message);
             }
 
@@ -56,10 +55,10 @@ class MessageController extends Controller
 
     }
 
-    public function getNewMessages($chat_id, $user_id,$last_date)
+    public function getNewMessages($chat_id, $user_id, $last_date)
     {
 
-        $messages = \App\Message::where('chat_id', $chat_id)->where('sender_user_id', '!=', $user_id)->where('is_seen', false)->where('created_at','>',$last_date)->get();
+        $messages = \App\Message::where('chat_id', $chat_id)->where('sender_user_id', '!=', $user_id)->where('is_seen', false)->where('created_at', '>', $last_date)->get();
 
 
         if (count($messages)) {
@@ -84,7 +83,7 @@ class MessageController extends Controller
 
     }
 
-    public function setUserTypingValue($request)
+    public function setUserTypingValue(Request $request)
     {
 
         $validator = Validator::make($request->all(), [
@@ -95,11 +94,11 @@ class MessageController extends Controller
         $validator->validate();
 
 
-        $chat = Chat::where('id', $request->chat_id)->first();
+        $chat = Chat::where('id', request('chat_id'))->first();
 
-        if ($chat->owner_user_id == $request->user_id) {
+        if ($chat->owner_user_id == request('user_id')) {
             $chat->is_owner_typing = $request->value;
-        } else if ($request->chat_id->guest_user_id == $request->user_id) {
+        } else if ($chat->guest_user_id == request('user_id')) {
             $chat->is_guest_typing = $request->value;
         }
 
@@ -111,7 +110,7 @@ class MessageController extends Controller
     }
 
 
-    public function sendMessage($request)
+    public function sendMessage(Request $request)
     {
 
         $validator = Validator::make($request->all(), [
@@ -122,10 +121,11 @@ class MessageController extends Controller
         ]);
         $validator->validate();
 
-        $input['chat_id'] = $request->chat_id;
-        $input['sender_user_id'] = $request->user_id;
-        $input['type'] = $request->type;
-        $input['message'] = $request->message;
+
+        $input['chat_id'] = request('chat_id');
+        $input['sender_user_id'] = request('user_id');
+        $input['type'] = request('type');
+        $input['message'] = request('message');
 
         $message = \App\Message::create($input);
 
@@ -142,13 +142,14 @@ class MessageController extends Controller
     {
 
         $message = \App\Message::where('id', $message_id)->first();
-        return response()->json(["status" =>$message->is_seen], 200);
+        return response()->json(["status" => $message->is_seen], 200);
 
 
     }
 
 
-    public function setIsMessageSeenValue($request){
+    public function setIsMessageSeenValue(Request $request)
+    {
 
         $validator = Validator::make($request->all(), [
             'message_id' => 'required',
@@ -156,7 +157,7 @@ class MessageController extends Controller
         $validator->validate();
 
 
-        $message = \App\Message::where('id', $request->message_id)->first();
+        $message = \App\Message::where('id', request('message_id'))->first();
         $message->is_seen = true;
         $message->save();
         return response()->json("OK", 204);
