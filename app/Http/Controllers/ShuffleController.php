7c0dc54ddf60b862e449e7ba64c11350cc6fe1fb
kocsburak <burak_s96@hotmail.com>
2020-04-09 18:44:39 +0300

    Hatalar düzeldtildi

--- app/Http/Controllers/ShuffleController.php
+++ app/Http/Controllers/ShuffleController.php
@@ -13,21 +13,6 @@ class ShuffleController extends Controller
     public function shuffle($user_id,$count)
     {
 
-/*
-        $amIAlreadyMatched = Chat::where('guest_user_id', $user_id)->where('is_checked', false)->first();
-
-        if ($amIAlreadyMatched) {
-
-            $user = User::where("id",$amIAlreadyMatched->owner_user_id)->first();
-            $department = Department::where("id", $user->department_id)->first();
-            $user['department_name'] = $department->name;
-            Chat::where('owner_user_id', $amIAlreadyMatched->owner_user_id)->where('guest_user_id', $user_id)->update(['is_checked' => true]);
-            return response()->json($user, 200);
-
-        }
-*/
-        // else
-
         $events = Event::where("id",">",$count)->where("group", "<", 3)->where("group", ">=", 0)->where("user_id", "!=", $user_id)->get();
         $cc = 0;
         foreach ($events as $event) {
